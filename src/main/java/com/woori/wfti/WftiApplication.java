c65c3b9f421b6f46c7ae068d93674f79d78f9b8f
SujinJeong <lovesujin05173@gmail.com>
2022-06-19 19:38:01 +0900

    apply .gitignore in wfti 4

---
+++ src/main/java/com/woori/wfti/WftiApplication.java
@@ -0,0 +1,13 @@
+package com.woori.wfti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WftiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WftiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woori/wfti/WftiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woori.wfti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WftiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
