d53a5dde72027083efc9e1dcd1ec282b653671c2
SujinJeong <lovesujin05173@gmail.com>
2022-10-28 22:32:04 +0900

    New Controller test 및 user api 경로 변경

--- src/main/java/com/woori/wfti/api/controller/UserController.java
+++ src/main/java/com/woori/wfti/api/controller/UserController.java
@@ -9,23 +9,22 @@ import java.util.List;
 
 @RestController             // Json 형태 결과값을 반환 @ResponseBody 버전 업
 @RequiredArgsConstructor    // final 객체를 Constructor 주입 해줌 (Autowired 역할)
-@RequestMapping("/v1")      // version 1 API
+@RequestMapping("/v1/user")      // version 1 API
 public class UserController {
     private final UserRepository userRepository;
 
     /**
      * 회원 조회
      */
-    @GetMapping("user")
+    @GetMapping("selectAll")
     public List<UserEntity> findAllUser() {
         return userRepository.findAll();
     }
 
     /**
      * 회원 등록 (테스트용)
-     *
      */
-    @PostMapping("user")
+    @PostMapping("createUser")
     public UserEntity createUser() {
         final UserEntity user = UserEntity.builder()
                 .userEmpNo("20201464")
